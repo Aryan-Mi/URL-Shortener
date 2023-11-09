ec9b60163f4f5d91944271f80ecf0767738ba0c5
Aryan Mirzazadeh <54137005+Aryan-Mi@users.noreply.github.com>
2023-11-09 16:19:00 +0100

    Adjusted hero section to be responsive

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -9,14 +9,16 @@ function Hero() {
         src={heroImg}
         alt="Illustration of a person working at office"
       />
-      <h1 className="hero-title">
-        More than just <br /> shorter links
-      </h1>
-      <p className="hero-description">
-        Build your brand's recognition and get detailed insights on how your
-        links are performing
-      </p>
-      <button className="button btn-primary btn-round">Get Started</button>
+      <div className="hero-info">
+        <h1 className="hero-title">
+          More than just <br /> shorter links
+        </h1>
+        <p className="hero-description">
+          Build your brand's recognition and get detailed insights on how your
+          links are performing
+        </p>
+        <button className="button btn-primary btn-round">Get Started</button>
+      </div>
     </section>
   )
 }
