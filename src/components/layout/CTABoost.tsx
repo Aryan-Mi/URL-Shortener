82443e5048587c1165ab091917f16ae6fb501c71
Aryan Mirzazadeh <54137005+Aryan-Mi@users.noreply.github.com>
2023-11-12 18:24:31 +0100

    CTA is now responsive

--- src/components/layout/CTABoost.tsx
+++ src/components/layout/CTABoost.tsx
@@ -1,11 +1,9 @@
-// import React from 'react'
 import '@/styles/CTABoost.css'
-import boost_bg from '@assets/images/bg-boost-mobile.svg'
 
 export default function CTABoost() {
   return (
     <section className="cta">
-      <img className="cta-bg" src={boost_bg} />
+      {/* <img className="cta-bg" src={boost_bg_mobile} /> */}
       <h2>Boost your links today</h2>
       <button className="button btn-round btn-primary">Get Started</button>
     </section>
